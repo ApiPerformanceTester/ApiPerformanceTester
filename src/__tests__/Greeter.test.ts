0f1934e8f60a74eaa2d3c628c044d609d3a1e87f
Siddhant shah <Siddhantshah04@gmail.com>
2022-04-03 21:46:34 +0530

    fixes

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,4 +1,13 @@
-import { Greeter } from '../index';
-test('My Greeter', () => {
-  expect(Greeter('Carl')).toBe('Hello Carl');
+import { Num } from '../index';
+
+test('add', () => {
+  expect(new Num(5).add(new Num(6)).val()).toBe(11);
+});
+
+test('toString', () => {
+  expect(new Num(5).toString()).toBe('5');
+});
+
+test('addAll', () => {
+  expect(Num.addAll([new Num(5), new Num(2), new Num(13)]).val()).toBe(20);
 });
